64b132d4e7de7635b8e8f7a371e10b57d73e0bab
Abhijith R Nair <aswajith679@gmail.com>
2025-01-25 15:46:24 +0530

    intial

--- lib/domain/newAndhot/comingsoons/newandhotrepo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:netflix_spoof/domain/core/failures/main_failures.dart';
-import 'package:netflix_spoof/domain/newAndhot/comingsoons/models/new_and_hot.dart';
-
-abstract class NewandHotHazardRepo {
-  Future<Either<MainFailure, NewAndHotResp>> getupcoming();
-}
